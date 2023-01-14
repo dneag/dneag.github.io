d4230d91b6a0841f87d05344404255e51873d954
dneag <dneag@hotmail.com>
2023-01-14 13:36:54 -0500

    Add grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./portfolio/**/*.{html,js}'],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: ['./portfolio/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
